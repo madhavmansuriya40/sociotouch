5fdfe8c2fc5ebb3cf955cb92455d86167fee4d53
madhavmansuriya40 <51825933+madhavmansuriya40@users.noreply.github.com>
2020-01-14 15:36:36 +0530

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -393,7 +393,7 @@ router.post('/get_posts_all_frnd', function (req, resu, next) {
     // });
 
     var MongoClient = require('mongodb').MongoClient;
-    var url = "mongodb://127.0.0.1:27017/";
+    var url = "mongodb+srv://madhav:<madmax>@cluster0-s9imw.mongodb.net/test";
 
     MongoClient.connect(url, function (err, db) {
         if (err) throw err;
